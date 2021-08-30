d3898e998cd3104746cf9b9563655fee89e3c9e6
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-08-30 14:43:32 -0300

    (#1) Adicionando configurações para teste

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,12 +10,18 @@ module.exports = {
     dialect: "postgres",
   },
   test: {
-    username: "postgres",
-    password: "postgres",
-    database: "ordertanamesa_dev",
-    host: "localhost",
-    port: 5105,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     dialect: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   homolog: {
     username: process.env.POSTGRES_USER,
