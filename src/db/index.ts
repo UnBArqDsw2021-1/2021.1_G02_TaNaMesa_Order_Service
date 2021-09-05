be815891b7573d5b8421cec190c890b18d3f20bf
Tiago Rodrigues <tsrrodrigues@outlook.com>
2021-09-05 19:31:23 -0300

    (#98) Conectando models Client e Item ao banco

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize";
 import databaseConfig from "../config/database";
 import { OrderFactory, OrderStatic } from "../models/Order";
+import { ItemFactory, ItemStatic } from "../models/Item";
+import { ClientFactory, ClientStatic } from "../models/Client";
 
 console.log(databaseConfig[process.env.NODE_ENV]);
 
@@ -8,6 +10,8 @@ class Database {
   public connection: Sequelize;
 
   public order: OrderStatic;
+  public item: ItemStatic;
+  public client: ClientStatic;
 
   constructor(test: boolean) {
     this.init(test);
@@ -33,6 +37,12 @@ class Database {
 
         this.order = OrderFactory(this.connection);
         this.order.sync();
+
+        this.item = ItemFactory(this.connection);
+        this.item.sync();
+
+        this.client = ClientFactory(this.connection);
+        this.client.sync();
       })
       .catch(() => {
         console.log("😵‍💫❌ Erro ao conectar no Banco\n");
