5cd2316b5a58cab5ac0c7e073b594f95913e0555
danieldagerom <danielprimo9@gmail.com>
2021-09-14 10:48:37 -0300

    (#8) Traduz atributos para inglês

--- src/models/Table.ts
+++ src/models/Table.ts
@@ -12,7 +12,7 @@ import {
 
 interface TableAttributes {
   idMesa: number;
-  cpfGarcom: string;
+  cpfWaiter: string;
 }
 export interface TableModel extends Model<TableAttributes>, TableAttributes { }
 export class Table extends Model<TableModel, TableAttributes> { }
@@ -25,12 +25,12 @@ export function TableFactory(sequelize: Sequelize): TableStatic {
   return <TableStatic>sequelize.define(
     "tables",
     {
-      idMesa: {
+      idTable: {
         type: INTEGER,
         primaryKey: true,
         autoIncrement: true,
       },
-      cpfGarcom: {
+      cpfWaiter: {
         type: STRING(11),
         references: {model: 'employees', key: 'cpf'}
       },
