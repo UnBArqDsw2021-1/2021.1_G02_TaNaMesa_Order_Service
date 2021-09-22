fab439c9454b748b517f25b5ca44f05622a528ef
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-21 22:23:19 -0300

    (#17) Adicionando rotas de autenticação
    Co-authored-by: Abner Filipe <abner.f.c.r@hotmail.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+
 import orderRoutes from "./order";
 import tableRoutes from "./table";
-
 import employeeRoutes from "./employee";
 import clientRoutes from "./client";
 import itemRoutes from "./item";
+import authRoutes from "./auth";
+import ensureAuth from "../middlewares/ensureAuth";
 
 const routes = Router();
 
@@ -26,10 +28,11 @@ routes.get("/", (request, response) => {
   });
 });
 
-routes.use("/orders", orderRoutes);
-routes.use("/table", tableRoutes);
-routes.use("/employee", employeeRoutes);
-routes.use("/client", clientRoutes);
-routes.use("/item", itemRoutes);
+routes.use("/login", authRoutes);
+routes.use("/orders", ensureAuth, orderRoutes);
+routes.use("/table", ensureAuth, tableRoutes);
+routes.use("/employee", ensureAuth, employeeRoutes);
+routes.use("/client", ensureAuth, clientRoutes);
+routes.use("/item", ensureAuth, itemRoutes);
 
 export default routes;
