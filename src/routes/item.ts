381289fbb71de7b8decf7e20bf11c0d1a9f1d5b3
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-29 18:10:00 -0300

    (#22) Ajustando configuração do multer na controller dos itens

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,32 +1,21 @@
+/* eslint-disable */
 import { Router } from "express";
+import multer from "multer";
+import path from "path";
+
 import itemController from "../controllers/itemController";
 
-const path = require('path')
-const multer = require('multer')
-
-const checkFileType = (file, cb) => {
-  const filetypes = /jpeg|jpg|png/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (mimetype && extname) return cb(null, true);
-  else cb('Error: Os formatos permitidos sao jpeg/jpg/png!');
-}
-
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
+  destination(req, file, cb) {
+    cb(null, "./uploads");
   },
-  filename: function (req, file, cb) {
-    cb(null, 'item_' + req.params.id + '.' + file?.mimetype?.split('/')[1])
+  filename(req, file, cb) {
+    cb(null, `item_${req.params.id}.${file?.mimetype?.split("/")[1]}`);
   },
-  fileFilter: function (_req, file, cb) {
-    checkFileType(file, cb)
-  }
-})
+});
 
-const upload = multer({ storage: storage })
-const routes = Router()
+const upload = multer({ storage });
+const routes = Router();
 
 routes.post("/", (request, response) =>
   itemController.create(request, response)
@@ -48,7 +37,7 @@ routes.delete("/:id", (request, response) =>
   itemController.destroy(request, response)
 );
 
-routes.put("/:id/image", upload.single('image'), (request, response) =>
+routes.put("/:id/image", upload.single("image"), (request, response) =>
   itemController.uploadPhoto(request, response)
 );
 
